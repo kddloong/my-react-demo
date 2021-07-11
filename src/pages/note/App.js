e7689f930331ee639043c51364692f40d21484cd
cjf <jltzjcf@163.com>
2021-07-11 16:07:58 +0800

    add ant-design-mobile

--- src/pages/note/App.js
+++ src/pages/note/App.js
@@ -1,11 +1,15 @@
 import logo from "../../logo.svg";
 import "./App.css";
+import { Button } from 'antd-mobile';
+import React from 'react';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Button size='small'>dasd</Button>
+
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
--- src/pages/note/index.js
+++ src/pages/note/index.js
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "../../reportWebVitals";
+import 'antd-mobile/dist/antd-mobile.less';
+
 
 ReactDOM.render(
   <React.StrictMode>
